25fa80a59102f0e32bcb0fc8e0b6957ae92d1056
estill01 <ehs.stillman@gmail.com>
2012-03-05 02:59:28 -0600

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -4,14 +4,14 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  # Bundler.require(*Rails.groups(:assets => %w(development test)))
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  Bundler.require(:default, :assets, Rails.env)
+  # Bundler.require(:default, :assets, Rails.env)
 end
 
 module Batsignal
   class Application < Rails::Application
-#    config.middleware.use Rack::Pjax
+#   config.middleware.use Rack::Pjax
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -59,6 +59,8 @@ module Batsignal
     # Enable the asset pipeline
     config.assets.enabled = true
 
+#    config.assets.precompile += ['glyphicons-halflings.png', 'glyphicons-halflings-white.png']
+ 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
